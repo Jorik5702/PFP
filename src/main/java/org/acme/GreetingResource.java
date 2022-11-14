97f97a99f8dd7bca2897a976215aabf01e852e20
Jörg Bächtiger <joerg.baechtiger@finnova.com>
2022-11-14 13:15:43 +0100

    change

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -2,17 +2,25 @@ package org.acme;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.logging.Logger;
 
 @Path("/hello")
 public class GreetingResource {
+
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class.getName());
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String hello(
             @DefaultValue ("100") @QueryParam("rounds") int maxRounds
     ) {
+        LOG.info("processin request with rounds="+maxRounds);
+
         double intermediate = 0.0;
         for(int round = 0; round < maxRounds; round++) {
+            for(int inner = 0; inner < 1000000; inner++) {
+                intermediate += Math.sqrt(round+inner);
+            }
             intermediate += Math.sqrt(round);
         }
         return "{" +
